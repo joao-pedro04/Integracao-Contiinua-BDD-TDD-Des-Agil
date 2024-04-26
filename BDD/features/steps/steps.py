75610fa36f0f86a0ccc25979a1ff84c24c1b16de
joao-pedro04 <107186628+joao-pedro04@users.noreply.github.com>
2024-04-25 21:29:48 -0300

    Update steps.py

--- BDD/features/steps/steps.py
+++ BDD/features/steps/steps.py
@@ -1,1 +1,15 @@
-from behave import given , when, then
+from behave import given, when, then
+from steps import calcular_media
+
+@given('que eu tenho dois números')
+def step_impl(context):
+    context.numero1 = 10
+    context.numero2 = 20
+
+@when('eu calculo a média desses números')
+def step_impl(context):
+    context.media = calcular_media(context.numero1, context.numero2)
+
+@then('eu devo obter a média correta')
+def step_impl(context):
+    assert context.media == 15
